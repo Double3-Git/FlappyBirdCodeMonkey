b0c7bf769ce014fd51853db917a70e8c710a1293
Double 3 <double3.2017@gmail.com>
2020-06-22 08:10:20 +0300

    Assets holder for easy inCode use

---
+++ Assets/Scripts/GameAssets.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAssets : MonoBehaviour
+{
+    [Header("Sprites")]
+    public Sprite pipeHeadSprite;
+
+    private static GameAssets instance;
+
+    public static GameAssets GetInstance() => instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
+}
