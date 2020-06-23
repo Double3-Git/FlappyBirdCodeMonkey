913a1893ddaf008a933bbd4f12266281b42bc1b8
Double 3 <double3.2017@gmail.com>
2020-06-23 22:23:35 +0300

    Top pipe create ability

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -11,21 +11,24 @@ public class Level : MonoBehaviour
 
     private void Start()
     {
-        CreatePipe(50f, 0f);
-        CreatePipe(40f, 20f);
-        CreatePipe(30f, 40f);
-        CreatePipe(20f, 60f);
+        CreatePipe(50f, 0f, true);
+        CreatePipe(50f, 0f, false);
+        CreatePipe(40f, 20f, true);
+        CreatePipe(30f, 40f, true);
+        CreatePipe(20f, 60f, true);
     }
 
-    void CreatePipe(float height, float xPosition)
+    void CreatePipe(float height, float xPosition, bool isBottom)
     {
+        int multipler = isBottom ? 1 : -1;
         // Setup pipe Head
         Transform pipeHead = Instantiate(GameAssets.GetInstance().pfPipeHead);
-        pipeHead.position = new Vector3(xPosition, -CAMERA_ORTHO_SIZE + height - PIPE_HEAD_HEIGHT * .5f);
+        pipeHead.position = new Vector3(xPosition, multipler * (- CAMERA_ORTHO_SIZE + height - PIPE_HEAD_HEIGHT * .5f));
 
         // Setup pipe Body
         Transform pipeBody = Instantiate(GameAssets.GetInstance().pfPipeBody);
-        pipeBody.position = new Vector3(xPosition, -CAMERA_ORTHO_SIZE);
+        pipeBody.position = new Vector3(xPosition, multipler * -CAMERA_ORTHO_SIZE);
+        pipeBody.localScale = new Vector3(1, multipler, 1);
         SpriteRenderer pipeBodySR = pipeBody.GetComponent<SpriteRenderer>();
         pipeBodySR.size = new Vector2(PIPE_WIDTH, height);
         BoxCollider2D pipeBodyBC2D = pipeBody.GetComponent<BoxCollider2D>();
